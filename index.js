99c22bc242f180126ad9da23499ea58278a19253
Barakat Akinrinde <barakatakinrinde@gmail.com>
2025-03-20 12:53:24 +0100

    Adjusted routing

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const connectToDB = require('./src/config/db');
 const mainRouter = require('./src/routes/mainRoutes');
-const productRouter = require('./src/routes/productRoutes');
 require('dotenv/config');
 
 const PORT = process.env.PORT || 8000;
@@ -13,7 +12,6 @@ connectToDB();
 app.use(express.json());
 
 app.use('/api/v1/trackventory', mainRouter);
-app.use('/api/v1/trackventory', productRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running at: localhost:${PORT}/api/v1/trackventory`);
--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -1,8 +1,10 @@
 const userRouter = require('./userRoutes');
+const productRouter = require('./src/routes/productRoutes');
 const express = require('express');
 
 const router = express.Router();
 
 router.use('/users', userRouter);
+router.use('/products', productRouter);
 
 module.exports = router;
